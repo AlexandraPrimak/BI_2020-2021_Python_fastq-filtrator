b1cc6a45ea6f082f1429ae4b0e663f1b1aa6014d
Александра Примак <alexandraprimak@MacBook-Pro-Aleksandra.local>
2020-11-29 17:07:19 +0300

    fix pep8 lint

--- filter_fastq.py
+++ filter_fastq.py
@@ -1,11 +1,13 @@
 import sys
 
+
 def count_GC(fastq_read):
     count = 0
     for base in fastq_read:
         if base == 'G' or base == 'C':
             count += 1
     return count * 100 / len(fastq_read)
+
 
 def write_read_to_file(fastq_read_lines, file):
     file.write('\n'.join(fastq_read_lines) + '\n')
@@ -18,7 +20,9 @@ def validate_read(fastq_read, min_length, gc_bounds):
         if count_GC(fastq_read) < gc_bounds[0]:
             return False
     if len(gc_bounds) == 2:
-        if count_GC(fastq_read) < gc_bounds[0] or count_GC(fastq_read) > gc_bounds[1]:
+        isLess = count_GC(fastq_read) < gc_bounds[0]
+        isMore = count_GC(fastq_read) > gc_bounds[1]
+        if isLess or isMore:
             return False
     return True
 
@@ -30,15 +34,19 @@ KEEP_FILTERED_ARG = '--keep_filtered'
 GC_BOUNDS_ARG = '--gc_bounds'
 OUTPUT_BASE_NAME_ARG = '--output_base_name'
 
-valid_args = [MIN_LENGTH_ARG, KEEP_FILTERED_ARG, GC_BOUNDS_ARG, OUTPUT_BASE_NAME_ARG]
-
+valid_args = [ 
+    MIN_LENGTH_ARG,
+    KEEP_FILTERED_ARG,
+    GC_BOUNDS_ARG,
+    OUTPUT_BASE_NAME_ARG
+]
 
 min_length = 0
 keep_filtered = False
 gc_bounds = []
 input_file = sys.argv[-1]
 
-output_base_name = '.'.join(splitted_by_dots[0 : len(splitted_by_dots) - 1])
+output_base_name = '.'.join(splitted_by_dots[0:len(splitted_by_dots) - 1])
 
 i = 1
 while i < len(sys.argv) - 1:
@@ -60,7 +68,8 @@ while i < len(sys.argv) - 1:
             gc_bounds.append(int(sys.argv[i]))
             i += 1
         if len(gc_bounds) > 2:
-            raise ValueError(GC_BOUNDS_ARG + ' must contain only up to two values')
+            errorText = GC_BOUNDS_ARG + ' must contain only up to two values'
+            raise ValueError(errorText)
         continue
     if sys.argv[i] == OUTPUT_BASE_NAME_ARG:
         output_base_name = sys.argv[i + 1]
@@ -69,9 +78,9 @@ while i < len(sys.argv) - 1:
 
 print('\n ----->>  Your setup: \n')
 print(MIN_LENGTH_ARG + ' ' + str(min_length))
-print(KEEP_FILTERED_ARG, keep_filtered, sep = " ")
-print(GC_BOUNDS_ARG, gc_bounds, sep = " ")
-print(OUTPUT_BASE_NAME_ARG + ' ' +  output_base_name)
+print(KEEP_FILTERED_ARG, keep_filtered, sep=" ")
+print(GC_BOUNDS_ARG, gc_bounds, sep=" ")
+print(OUTPUT_BASE_NAME_ARG + ' ' + output_base_name)
 print('your input file name: ', input_file)
 print('\n ----->> Filtering: Start')
 
@@ -85,7 +94,7 @@ lines = fastq.read().splitlines()
 count_passed = 0
 count_failed = 0
 for i in range(0, len(lines), 4):
-    fastq_read_info = lines[i : i + 4]
+    fastq_read_info = lines[i:i + 4]
     if i == 0:
         print()
     if validate_read(fastq_read_info[1], min_length, gc_bounds):
@@ -96,7 +105,10 @@ for i in range(0, len(lines), 4):
             write_read_to_file(fastq_read_info, failed)
         count_failed += 1
 
-print('\n ----->> total number of processed reads in ' + input_file + ': ' + str(len(lines) // 4))
+total_reads_number_str = str(len(lines) // 4)
+total_number_text = '\n ----->> total number of processed reads in '
+
+print(total_number_text + input_file + ': ' + total_reads_number_str)
 print('\n ----->> Filtering: Finish')
 print('\n ----->> number of passed reads: ' + str(count_passed))
 print('\n ----->> number of failed reads: ' + str(count_failed))
